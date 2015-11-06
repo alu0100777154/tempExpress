0d5bc96d2fedce3034c2760222e0be32862705c8
alu0100777154 <alu0100777154@ull.edu.es>
2015-11-06 06:55:57 +0000

    instalado express-ejs-layouts y cambios en server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 // load the things we need
 var express = require('express');
 var app = express();
+var path = require('path');
+var temperatura = require("./temperature.js");
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+var expressLayouts = require('express-ejs-layouts');
 
 // use res.render to load up an ejs view file
 
